54b4f19431f2071c80b4cf6d00ec057e02282fec
Mickey0521 <smallfriend521@gmail.com>
2017-09-02 16:23:01 +0800

    Create 12_Integer_to_Roman.java

---
+++ 12_Integer_to_Roman.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String intToRoman(int num) {
+        
+        // store all the possible "numbers" and "strings"
+        int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        
+        // Using "StringBuilder" (important)
+        StringBuilder string_builder = new StringBuilder();
+        
+        // for each possible nums (from big one to small one)
+        for(int i=0; i < nums.length; i++){
+            // using while loop -> "num = num - nums[i]"
+            // note: if "num == nums[i]" -> also "num = num - nums[i]" (be careful)
+            while( num >= nums[i]){
+                num = num - nums[i];
+                // Using "StringBuilder.append(str)"
+                string_builder.append(roman[i]); 
+            }
+        }
+        
+        return string_builder.toString(); // Using "toString()"
+    }
+}
